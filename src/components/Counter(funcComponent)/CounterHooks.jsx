ca9a8d9dc22dde13ce55b943bd9d5eaa464bce8b
Oleg <ikslprint@gmail.com>
2024-02-20 10:55:54 +0200

    practice useReducer  in Counter

--- src/components/Counter(funcComponent)/CounterHooks.jsx
+++ src/components/Counter(funcComponent)/CounterHooks.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 
 import {
   CounterWrap,
@@ -6,29 +6,47 @@ import {
   CounterControls,
 } from "./CounterHooks.styled";
 
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + action.payload;
+    case "decrement":
+      return state - action.payload;
+    default:
+      return state;
+  }
+};
+
 export const CounterHooks = () => {
-  const [value, setValue] = useState(5);
+  // const [value, setValue] = useState(5);
 
-  const handleIncrement = () => {
-    setValue((state) => state + 1);
-  };
+  const [count, dispatch] = useReducer(countReducer, 0);
 
-  const handleDecrement = () => {
-    setValue((state) => state - 1);
-  };
+  // const handleIncrement = () => {
+  //   setValue((state) => state + 1);
+  // };
 
-  useEffect(() => {
-    document.title = `Value counter is ${value}`;
-  }, [value]);
+  // const handleDecrement = () => {
+  //   setValue((state) => state - 1);
+  // };
+
+  // useEffect(() => {
+  //   document.title = `Value counter is ${count}`;
+  // }, [count]);
 
   return (
     <CounterWrap>
-      <CounterValue>{value}</CounterValue>
+      <CounterValue>{count}</CounterValue>
       <CounterControls className="Counter__controls">
-        <button type="button" onClick={handleDecrement}>
+        <button
+          type="button"
+          onClick={() => dispatch({ type: "decrement", payload: 1 })}
+        >
           Decrement
         </button>
-        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={() => dispatch({ type: "increment", payload: 1 })}>
+          Increment
+        </button>
       </CounterControls>
     </CounterWrap>
   );
